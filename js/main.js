255e20bdc697182df4f1586e24ac30a3582bd891
Hasan <86289228+hasanglv@users.noreply.github.com>
2023-05-21 23:14:43 +0400

    testimonials and company

--- js/main.js
+++ js/main.js
@@ -21,3 +21,27 @@ function closeDropdown() {
     dropdown.style.opacity = "0";
 }
 
+var swiper = new Swiper(".mySwiper", {
+    grabCursor: true,
+    effect: "creative",
+    creativeEffect: {
+        prev: {
+            shadow: true,
+            origin: "left center",
+            translate: ["-5%", "-100%", -100],
+            rotate: [100, 0, 0],
+        },
+        next: {
+            origin: "right center",
+            translate: [55, 95, -100],
+        },
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
